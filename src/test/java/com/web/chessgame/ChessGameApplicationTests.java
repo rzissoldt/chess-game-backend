e0be09f24bad8e39436956adc99520ccc385e597
Ruben Zissoldt <rzissoldt@gmail.com>
2020-05-29 21:08:11 +0200

    Created Repo

---
+++ src/test/java/com/web/chessgame/ChessGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.chessgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
